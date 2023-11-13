b23d6c862a161d79bf0a97b10df31e1f7a9f3df2
Troy <73727463+Yort123@users.noreply.github.com>
2023-11-13 13:00:09 -0500

    Create  toDo.java
    Java FX skeleton

---
+++ toDo.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TableColumn<?, ?> completed;
+
+    @FXML
+    private TableColumn<?, ?> inProgress;
+
+    @FXML
+    private TableColumn<?, ?> toDo;
+
+}

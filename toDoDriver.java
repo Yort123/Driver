b514d70eaa4006a41a48e4111a718fb84bfc2078
sidTsui <147187017+sidTsui@users.noreply.github.com>
2023-11-26 00:25:47 -0500

    Update toDoDriver.java

--- toDoDriver.java
+++ toDoDriver.java
@@ -1,13 +1,13 @@
-package toDoList;
+package toDoList;// organizes and connects different classes within the project
 
-public class toDoDriver {
+public class toDoDriver {// driver meant to to test the project 
     static ToDoList activity1 = new ToDoList("CS Lab", "11/8", 5);
 
     public static void main(String[] args) {
         System.out.println("Task 1");
         System.out.println(activity1.getActivity());
         System.out.println(activity1.getCompletedByDate());
-        System.out.println(activity1.getUrgencyLevel());
+        System.out.println(activity1.getUrgencyLevel());//prints out the results when calling other methods to be tested
 
 
     }

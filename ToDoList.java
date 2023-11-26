69f01514c655806692788e5919d62a24791e4b28
sidTsui <147187017+sidTsui@users.noreply.github.com>
2023-11-26 00:04:11 -0500

    Update ToDoList.java

--- ToDoList.java
+++ ToDoList.java
@@ -1,22 +1,22 @@
 package toDoList;
 
-public class ToDoList {
-    private String activity;
+public class ToDoList { 
+    private String activity; //establishing private instance variables
     private String completedByDate;
     private int urgencyLevel; //1-5
 
     public ToDoList(String activity, String completedByDate, int urgencyLevel){
-        this.activity = activity;
+        this.activity = activity; // using "this" to differentiate between instance variables and method parameters when assigning
         this.completedByDate = completedByDate;
         this.urgencyLevel = urgencyLevel;
     }
     //getters
-    public String getActivity(){return activity;}
+    public String getActivity(){return activity;} //implementing accessors 
     public String getCompletedByDate(){return completedByDate;}
     public int getUrgencyLevel(){return urgencyLevel;}
 
     //setters
-    public void setActivity(String activity){this.activity = activity;}
+    public void setActivity(String activity){this.activity = activity;} // implementing mutators 
     public void setCompletedByDate(String completedByDate){this.completedByDate = completedByDate;}
     public void setUrgencyLevel(int urgencyLevel){this.urgencyLevel = urgencyLevel;}
 }
